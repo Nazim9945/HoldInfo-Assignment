ad8e31fa845b2889fc6908790331e2a440ccf83e
Nazim Saifi <saifinazim2311@gmail.com>
2024-09-17 07:31:30 +0530

    ready to run

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 require('dotenv').config()
 const express = require('express');
 const app = express();
-
+const pg=require('pg')
+const mongoose=require('mongoose')
+const dbschema=require('./model/newschema')
 
 app.use(express.json())
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 
-const port=process.env.PORT
+const PORT=process.env.PORT
+const MONGODB_URL=process.env.MONGODB_URL
+const connection = async()=>{
+
+    try {
+        await mongoose.connect(MONGODB_URL);
+            console.log("database connected");
+    } catch (error) {
+        console.log("database conenction error");
+    }
+}
+connection();
 
 
 app.get('/', async (req, res) => {
@@ -15,7 +28,7 @@ app.get('/', async (req, res) => {
       const response = await fetch('https://api.wazirx.com/api/v2/tickers');
       const data = await response.json();
       const responsedata = Object.values(data).slice(0, 10);
-      console.log(responsedata);
+      // console.log(responsedata);
       res.render('allinone/home',{data:responsedata});
     } catch (error) {
       console.error(error);
@@ -23,9 +36,23 @@ app.get('/', async (req, res) => {
     }
   });
 
+app.post("/",async(req,res)=>{
+  
+
+try {
+  const data=await dbschema.create({name:"Nine tail"})
+    console.log("data saved");
+    return res.status(200).json({
+      msg:data
+    })
+} catch (error) {
+  console.log(error)
+   res.status(404).json({messege:"error found"}); 
+}
+});
 
 
 
-app.listen(port,()=>{
-    console.log(`server is listening at http://localhost:${port}`)
+app.listen(PORT,()=>{
+    console.log(`server is listening at http://localhost:${PORT}`)
 });
